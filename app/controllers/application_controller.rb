becb1717def76ad0e7bdf3cd6f66730b3684027f
DrVonDevious <drvondevious@gmail.com>
2020-03-02 20:07:09 -0600

    add authentication and static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authorized
+  helper_method :current_user
+  helper_method :logged_in?
+
+  def set_object(model)
+    model.find(params[:id])
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authorized
+    redirect_to '/' if !logged_in?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Session routes
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  get '/authorized', to: 'sessions#page_requires_login'
+
+  # User Routes
+  resources :users, only: [:new, :create]
+
+  # Static routes
+  root 'static#home'
+  get 'static/about'
+
 end
